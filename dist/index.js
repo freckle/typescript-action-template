1b7a584129c3005960e5c53302492192ac5cd745
patrick brisbin <pbrisbin@gmail.com>
2023-03-31 15:11:46 -0400

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -68,21 +68,16 @@ function run() {
         var token;
         return __generator(this, function (_a) {
             try {
-                core.startGroup("Inputs");
                 token = core.getInput("github-token", { required: true });
-                core.info("Action loaded");
             }
             catch (error) {
                 if (error instanceof Error) {
-                    core.error(error);
                     core.setFailed(error.message);
                 }
                 else if (typeof error === "string") {
-                    core.error(error);
                     core.setFailed(error);
                 }
                 else {
-                    core.error("Non-Error exception");
                     core.setFailed("Non-Error exception");
                 }
             }
