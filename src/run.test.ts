071e7d6c361fdf5f7b2611822061f2670ea65026
patrick brisbin <pbrisbin@gmail.com>
2026-07-28 10:28:11 -0400

    fix: add example github-client

--- src/run.test.ts
+++ src/run.test.ts
@@ -1,5 +1,6 @@
 import * as core from "@actions/core";
 
+import { PullRequest, GitHub } from "./github-client.js";
 import { run } from "./run.js";
 
 vi.mock(import("@actions/core"), () => {
@@ -9,13 +10,35 @@ vi.mock(import("@actions/core"), () => {
 });
 
 describe("run", () => {
-  it("logs the given context and inputs", () => {
-    const context = { eventName: "pull_request" };
+  it("logs the given context and inputs", async () => {
+    const context = {
+      eventName: "pull_request",
+      owner: "freckle",
+      repo: "action-name",
+      pullRequestNumber: 42,
+    };
+
     const inputs = { token: "_token_" };
 
-    run(context, inputs);
+    const pullRequest = {
+      number: 42,
+      title: "Awesome PR",
+    };
 
-    expect(core.info).toHaveBeenCalledWith('{"eventName":"pull_request"}');
-    expect(core.info).toHaveBeenCalledWith('{"token":"_token_"}');
+    const client = {
+      getPullRequest: async (
+        _owner: string,
+        _repo: string,
+        _pull_number: number,
+      ): Promise<PullRequest> => {
+        return pullRequest;
+      },
+    };
+
+    await run(context, inputs, client);
+
+    expect(core.info).toHaveBeenCalledWith(JSON.stringify(context));
+    expect(core.info).toHaveBeenCalledWith(JSON.stringify(inputs));
+    expect(core.info).toHaveBeenCalledWith(JSON.stringify(pullRequest));
   });
 });
